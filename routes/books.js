c4541c566db1757a50f4d37de0b50b6fa0c75920
denniseitner <denniseitner@gmail.com>
2018-08-14 15:22:38 +0200

    books detail form working
    Signed-off-by: denniseitner <denniseitner@gmail.com>

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Books = require('../models').books;
+var Book = require('../models').books;
 
 /* GET books listing. */
 
 router.get('/', function(req, res, next) {
-	Books.findAll({
+	Book.findAll({
 		attributes: ['id', 'title', 'author', 'genre', 'first_published']
 	}).then(books => {
 		res.render('all_books', { books: books });
@@ -15,9 +15,20 @@ router.get('/', function(req, res, next) {
 router.get('/new_book', function(req, res) {
 	res.render('new_book');
 });
-
+/*
 router.get('/:id', function(req, res, next) {
-	res.render('book_detail');
+//	res.render('book_detail');
+});
+*/
+/* GET detail book. */
+router.get('/:id', function(req, res) {
+	console.log(req.params.id);
+	Book.findOne({
+		where: { id: req.params.id },
+		attributes: ['id', 'title', 'author', 'genre', 'first_published']
+	}).then(book => {
+		res.render('book_detail', { book: book });
+	});
 });
 
 module.exports = router;
