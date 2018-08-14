94d3882568dd0a9e5c67576ac438e0de6d0ad6d0
denniseitner <denniseitner@gmail.com>
2018-08-14 14:38:42 +0200

    all books listing working
    Signed-off-by: denniseitner <denniseitner@gmail.com>

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Books = require('../models').books;
 
 /* GET books listing. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function(req, res) {
 	res.render('all_books');
 });
-router.get('/all_books', function(req, res, next) {
-	res.render('all_books');
+router.get('/new_book', function(req, res) {
+	res.render('new_book');
 });
+
+router.get('/all_books', function(req, res) {
+	Books.findAll({
+		attributes: ['id', 'title', 'author', 'genre', 'first_published']
+	}).then(books => {
+		res.render('all_books', { books: books });
+	});
+});
+
 module.exports = router;
