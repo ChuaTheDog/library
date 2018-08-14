0df4ddb52dd5ffb11b8d767b10000b4b7d1a2db9
denniseitner <denniseitner@gmail.com>
2018-08-14 14:42:37 +0200

    books routes fixed
    Signed-off-by: denniseitner <denniseitner@gmail.com>

--- routes/books.js
+++ routes/books.js
@@ -4,19 +4,16 @@ var Books = require('../models').books;
 
 /* GET books listing. */
 
-router.get('/', function(req, res) {
-	res.render('all_books');
-});
-router.get('/new_book', function(req, res) {
-	res.render('new_book');
-});
-
-router.get('/all_books', function(req, res) {
+router.get('/', function(req, res, next) {
 	Books.findAll({
 		attributes: ['id', 'title', 'author', 'genre', 'first_published']
 	}).then(books => {
 		res.render('all_books', { books: books });
 	});
+});
+
+router.get('/new_book', function(req, res) {
+	res.render('new_book');
 });
 
 module.exports = router;
