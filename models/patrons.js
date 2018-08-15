4d737b12063dd129cc13eba9c36e27b8a9b81c39
denniseitner <denniseitner@gmail.com>
2018-08-15 11:53:30 +0200

    patrons update working
    Signed-off-by: denniseitner <denniseitner@gmail.com>

--- models/patrons.js
+++ models/patrons.js
@@ -1,15 +1,66 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var patrons = sequelize.define('patrons', {
-    first_name: DataTypes.TEXT,
-    last_name: DataTypes.TEXT,
-    address: DataTypes.TEXT,
-    email: DataTypes.TEXT,
-    library_id: DataTypes.TEXT,
-    zip_code: DataTypes.INTEGER
-  }, {});
-  patrons.associate = function(models) {
-    // associations can be defined here
-  };
-  return patrons;
+	var patrons = sequelize.define(
+		'patrons',
+		{
+			first_name: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: 'first name is required'
+					}
+				}
+			},
+			last_name: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: 'last name is required'
+					}
+				}
+			},
+			address: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: 'address is required'
+					}
+				}
+			},
+			email: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notEmpty: {
+						msg: 'email is required'
+					}
+				}
+			},
+			library_id: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: 'library ID is required'
+					}
+				}
+			},
+			zip_code: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notEmpty: {
+						msg: 'zip_code is required'
+					},
+					isNumeric: {
+						msg: 'not a number'
+					}
+				}
+			}
+		},
+		{
+			timestamps: false
+		}
+	);
+	patrons.associate = function(models) {
+		// associations can be defined here
+	};
+	return patrons;
 };
--- routes/patrons.js
+++ routes/patrons.js
@@ -19,6 +19,7 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/*get detail patron*/
 router.get('/:id', function(req, res) {
 	Patron.findOne({
 		where: { id: req.params.id },
@@ -33,6 +34,39 @@ router.get('/:id', function(req, res) {
 		]
 	}).then(patron => {
 		res.render('patron_detail', { patron: patron });
+	});
+});
+
+/*update detail patron*/
+
+router.post('/:id', function(req, res, next) {
+	//console.log(req.body);
+	const updates = req.body.title;
+	Patron.findById(req.params.id).then(patron => {
+		patron
+			.updateAttributes({
+				first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				address: req.body.address,
+				email: req.body.email,
+				library_id: req.body.library_id,
+				zip_code: req.body.zip_code
+			})
+			.catch(function(error) {
+				if (error.name === 'SequelizeValidationError') {
+					console.log(error.name);
+					res.render('patron_detail', {
+						patron: Patron.build(req.body),
+						errors: error.errors,
+						title: 'New Article'
+					});
+				} else {
+					throw error;
+				}
+			})
+			.then(() => {
+				res.redirect('/patrons/' + req.params.id);
+			});
 	});
 });
 
