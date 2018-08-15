54745f13eec83f2195c7edb219f1e7d6bfceea50
denniseitner <denniseitner@gmail.com>
2018-08-15 11:29:47 +0200

    all patrons table working
    Signed-off-by: denniseitner <denniseitner@gmail.com>

--- routes/patrons.js
+++ routes/patrons.js
@@ -4,7 +4,19 @@ var Patron = require('../models').patrons;
 
 /* GET patrons listing. */
 router.get('/', function(req, res, next) {
-	res.render('all_patrons');
+	Patron.findAll({
+		attributes: [
+			'id',
+			'first_name',
+			'last_name',
+			'address',
+			'email',
+			'library_id',
+			'zip_code'
+		]
+	}).then(patrons => {
+		res.render('all_patrons', { patrons: patrons });
+	});
 });
 
 module.exports = router;
